0ff1fc6fcc884947d39892abd9ca417859307670
Maedeh_Shchgh <maedeh.shahcheraghi1384@gmail.com>
2026-05-13 01:06:28 +0330

    feat(mino_n8n_services): Add External services.

---
+++ src/core/OrchestrationPlatform.Application/Abstractions/Models/ServiceModels/UploadResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrchestrationPlatform.Application.Abstractions.Models.ServiceModels
+{
+    public record UploadResult(
+        string BucketName,
+        string ObjectKey,
+        long FileSize,
+        string Sha256Hash
+    );
+}
---
+++ src/core/OrchestrationPlatform.Application/Abstractions/Services/External/IOrchestrationService.cs
@@ -0,0 +1,13 @@
+﻿namespace OrchestrationPlatform.Application.Abstractions.Services.External;
+
+public interface IOrchestrationService
+{
+    Task<string> TriggerInstallWorkflowAsync(
+        Guid operationId,
+        string hostIp,
+        string sshUsername,
+        string downloadUrl,
+        CancellationToken cancellationToken = default);
+
+    Task CancelWorkflowAsync(string externalWorkflowId, CancellationToken cancellationToken = default);
+}
